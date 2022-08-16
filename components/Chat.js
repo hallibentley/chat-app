48cc021be345dbdbee338a979c2d76903ef961ca
hallibentley <hallibentley@gmail.com>
2022-08-16 13:54:16 -0400

    added giftedchat

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,78 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
   componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello, developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
   }
 
   render() {
     const { color } = this.props.route.params;
 
     return (
-      <ScrollView style={{ backgroundColor: color }}>
+      <View style={{ backgroundColor: color, flex: 1 }}>
         <View style={styles.container}>
           <Text style={styles.text}>Chat Screen</Text>
         </View>
-      </ScrollView>
+        <View style={{ flex: 1 }}>
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            onSend={(messages) => this.onSend(messages)}
+
+            user={{
+              _id: 1,
+            }}
+          />
+          {Platform.OS === 'android' ? (<KeyboardAvoidingView behavior="height" />
+          ) : null}
+        </View>
+
+      </View>
     );
   }
 
@@ -24,8 +80,6 @@ export default class Chat extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center'
   },
